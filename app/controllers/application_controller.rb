94d52062685c085fcf87313215044a9a23127f48
mticciati <matthewticciati@gmail.com>
2017-03-26 15:35:54 -0700

    add devise and mili for mult-tenancy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_tenant!   # authenticate user and sets up tenant
+
+  rescue_from ::Milia::Control::MaxTenantExceeded,   :with => :max_tenants
+  rescue_from ::Milia::Control::InvalidTenantAccess, :with => :invalid_tenant
 end
